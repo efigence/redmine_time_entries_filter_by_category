7e062d4ff7d8170c65884f3240695ee46a811505
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2015-09-23 10:01:19 +0200

    fix init

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 Redmine::Plugin.register :redmine_time_entries_filter_by_category do
   name 'Redmine Time Entries Filter By Category plugin'
-  author 'Author name'
+  author 'Marcin Świątkiewicz'
   description 'This plugin lets users filter time entires by issue category.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/efigence/redmine_time_entries_filter_by_category'
   author_url 'http://efigence.com'
 end
 ActionDispatch::Callbacks.to_prepare do
